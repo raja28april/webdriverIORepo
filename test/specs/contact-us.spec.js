4197555ffee48901f191e3c13f86b7bf4769d27f
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-10-30 20:38:23 +0100

    Raja: generating a sample test requirement

---
+++ test/specs/contact-us.spec.js
@@ -0,0 +1,16 @@
+describe('webdriverUniversity contact us page', () => {
+    it('valid submission - submit all information', () => {
+        //first name
+        //last name
+        //email address
+        //message
+        //submit button
+    });
+
+    it('invalid submission - dont submit all information', () => {
+        //first name
+        //last name
+        //message
+        //submit button
+    });
+});
