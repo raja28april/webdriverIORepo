ecd427688f15c31854fed672b0f3ead07bbc6bf0
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-10-31 12:07:18 +0100

    Raja: using waitForclickable method on the element

--- test/specs/wait-commands.spec.js
+++ test/specs/wait-commands.spec.js
@@ -12,4 +12,12 @@ describe('', () => {
         await btn_clickMe.click();
         await browser.pause(1500);
     });
+
+    it.only('using waitFor methods', async () => {
+        const btn_clickMe = await $('#button1');
+        // await btn_clickMe.waitForClickable({ timeout: 3000 })
+        await btn_clickMe.waitForClickable();
+        await btn_clickMe.click();
+        await browser.pause(1500);
+    });
 });
